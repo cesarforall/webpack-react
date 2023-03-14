f229f8ad7e457433f3f8d39daa04d12cc1e294a8
César Almeida <cesaralmeida89@gmail.com>
2023-03-14 17:40:03 +0100

    :wrench: add html connfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+	mode: 'production',
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -19,8 +20,18 @@ module.exports = {
 					loader: 'babel-loader',
 				},
 			},
+			{
+				test: /\.html$/,
+				use: [{ loader: 'html-loader' }],
+			},
 		],
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html',
+		}),
+	],
 	devServer: {
 		static: {
 			directory: path.join(__dirname, 'public'),
