5f1bcb34949147ebfdb67e6952eb7e63b1254c10
César Almeida <cesaralmeida89@gmail.com>
2023-03-15 19:37:29 +0100

    :wrench: add css and styles config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	mode: 'production',
@@ -24,6 +25,10 @@ module.exports = {
 				test: /\.html$/,
 				use: [{ loader: 'html-loader' }],
 			},
+			{
+				test: /\.s[ac]ss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
 		],
 	},
 	plugins: [
@@ -31,12 +36,15 @@ module.exports = {
 			template: './public/index.html',
 			filename: './index.html',
 		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
 	],
-	devServer: {
-		static: {
-			directory: path.join(__dirname, 'public'),
-		},
-		compress: true,
-		port: 9000,
-	},
+	// devServer: {
+	// 	static: {
+	// 		directory: path.join(__dirname, 'public'),
+	// 	},
+	// 	compress: true,
+	// 	port: 9000,
+	// },
 };
